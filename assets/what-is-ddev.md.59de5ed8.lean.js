d51caac1c38a384ff29958660edfab0760dcc7f8
mandrasch <777278+mandrasch@users.noreply.github.com>
2022-06-03 11:03:45 +0000

    Deploying to gh-pages from @ mandrasch/my-ddev-lab@c10503ea7d394f238ab53644775ded9b310d3560 🚀

---
+++ assets/what-is-ddev.md.59de5ed8.lean.js
@@ -0,0 +1,1 @@
+import{_ as e,c as t,o as r,a}from"./app.ae8a6e4d.js";const f='{"title":"What is DDEV?","description":"","frontmatter":{},"headers":[],"relativePath":"what-is-ddev.md","lastUpdated":1654254200000}',o={},n=a("",7),s=[n];function d(i,l,h,p,c,m){return r(),t("div",null,s)}var v=e(o,[["render",d]]);export{f as __pageData,v as default};
