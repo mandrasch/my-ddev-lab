15931e1382e26ce072336d10c9efa2aba62a0182
mandrasch <777278+mandrasch@users.noreply.github.com>
2022-10-15 08:17:39 +0000

    Deploying to gh-pages from @ mandrasch/my-ddev-lab@8525302cabd749c3232cc71fe20adb683293a206 🚀

---
+++ assets/ddev-composer.md.a6ecd51a.lean.js
@@ -0,0 +1,1 @@
+import{_ as e,c as s,o as a,a as o}from"./app.b04d529e.js";const u=JSON.parse('{"title":"Composer","description":"","frontmatter":{},"headers":[],"relativePath":"ddev-composer.md","lastUpdated":1665821818000}'),r={name:"ddev-composer.md"},t=o("",8),p=[t];function n(c,l,d,i,m,h){return a(),s("div",null,p)}var y=e(r,[["render",n]]);export{u as __pageData,y as default};
